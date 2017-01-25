ce5ac0d6c095e4e7efe3c8979566210089d59237
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2017-01-25 15:35:44 +0100

    use IFSC packet

--- restore_par2.go
+++ restore_par2.go
@@ -16,6 +16,7 @@
 package main
 
 import (
+	"crypto/md5"
 	"hash/crc32"
 	"io"
 	"log"
@@ -39,20 +40,70 @@ func newPAR2NextShard(meta FileMetadata, parity namedReader, data io.Reader) fun
 		return func(_ []byte, _ int) (ShardMetadata, []byte, error) { return ShardMetadata{}, nil, err }
 	}
 
-	hsh := crc32.New(crc32cTable)
-	D := int(meta.DataShards)
+	hCRC := crc32.NewIEEE()
+	hMD5 := md5.New()
+	D, P := int(meta.DataShards), int(meta.ParityShards)
 
-	// FIXME(tgulacsi): data shards, then parity shards
+	totalSize := info.Files[0].FileDescPacket.FileLength
+	index, dataIndex := -1, -1
+	var got par2.ChecksumPair
 
 	return func(p []byte, i int) (ShardMetadata, []byte, error) {
-		log.Printf("i=%d", i)
-		rd := info.RecoveryData[i]
-		if rd.Damaged {
-			return ShardMetadata{}, nil, errors.Wrap(errRecoveryDataInvalid, strconv.Itoa(i+1))
+		if totalSize == 0 {
+			return ShardMetadata{}, nil, io.EOF
 		}
-		hsh.Reset()
-		hsh.Write(rd.RecoveryData)
-		sm := ShardMetadata{Index: uint32(i + 1), Size: uint32(info.TotalSize), Hash32: hsh.Sum32()}
+		index++
+		hCRC.Reset()
+		hMD5.Reset()
+		length := int(info.Main.BlockSize)
+		sm := ShardMetadata{Index: uint32(index + 1), Size: meta.ShardSize}
+
+		log.Println(i, totalSize)
+		if i < D {
+			dataIndex++
+			if totalSize < uint64(length) {
+				length = int(totalSize)
+			}
+			totalSize -= uint64(length)
+			sm.Size = uint32(length)
+			if sm.Size == 0 {
+				return sm, p, nil
+			}
+			n, err := io.ReadFull(io.TeeReader(data, io.MultiWriter(hMD5, hCRC)), p[:length])
+			if err != nil {
+				if sek, ok := data.(io.Seeker); ok {
+					if _, seekErr := sek.Seek(int64(len(p)-n), io.SeekCurrent); seekErr != nil {
+						return sm, nil, errors.Wrapf(err, "seek: %v", seekErr)
+					}
+					return sm, nil, errors.Wrap(errShardBroken, "missing slice")
+				}
+				return sm, nil, err
+			}
+			log.Println("read", n)
+
+			if length < len(p) {
+				zero(p[length:])
+				hCRC.Write(p[length:])
+				hMD5.Write(p[length:])
+			}
+			want := info.Files[0].IFSCPacket.Pairs[dataIndex]
+			hCRC.Sum(got.CRC32[:0])
+			hMD5.Sum(got.MD5[:0])
+			if want == got {
+				sm.Hash32 = hCRC.Sum32()
+				return sm, p, nil
+			}
+			err = errors.Wrapf(errShardBroken, "%d. shard crc/md5 mismatch (got %s, wanted %s)!", i, got, want)
+			log.Printf("%v", err)
+			return sm, nil, err
+		}
+		// parity
+		rd := info.RecoveryData[(index%(D+P))-D]
+		if rd.Damaged {
+			return sm, nil, errors.Wrap(errShardBroken, strconv.Itoa(i+1))
+		}
+		hCRC.Write(rd.RecoveryData)
+		sm.Hash32 = hCRC.Sum32()
 		return sm, rd.RecoveryData, nil
 	}
 }
